62a095176e0f8dbe57b9f8bae2f298bee130beb5
K00TYARA <mr.kotlov.201@gmail.com>
2025-09-14 21:08:34 +0300

    Add jump opportunity and groundCheck for only one jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,18 +2,53 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    // Components
     private Rigidbody2D rb;
 
+    // Movement
     private float xInput;
     [SerializeField] private float moveSpeed = 2;
+    [SerializeField] private float jumpForce = 8;
+
+    [Header("Collision check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
+
+        CollisionChecks();
+        HandleInput();
+
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
+    }
+
+    private void HandleInput() {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
+        if (Input.GetKey(KeyCode.Space) && isGrounded) {
+            Jump();
+        }
+    }
+
+    private void Jump() {
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+    }
+
+    private void CollisionChecks() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
