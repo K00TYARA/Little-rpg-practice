6ae7cf4c138f9dff536e1d192b6e9189c0f47dac
K00TYARA <mr.kotlov.201@gmail.com>
2025-09-21 14:59:47 +0300

    Add damage cooldowns

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,31 @@
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
-    
+
+    private SpriteRenderer sr;
+
+    [SerializeField] private float RedColorDuration = 1;
+    public float lastTimeDamaged;
+
+    private Color thisColor;
+
+    private void Awake() {
+        sr = GetComponent<SpriteRenderer>();
+        thisColor = sr.color;
+    }
+
+    private void Update() {
+        ChangeColorIfNeeded();
+    }
+
+    private void ChangeColorIfNeeded() {
+        if (sr.color == Color.red && Time.time > lastTimeDamaged + RedColorDuration)
+            sr.color = thisColor;
+    }
+
     public void TakeDamage() {
-        Debug.Log($"{gameObject.name}: -100 hp");
+        sr.color = Color.red;
+        lastTimeDamaged = Time.time;
     }
 
 }
